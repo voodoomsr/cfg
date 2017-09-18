2766ec2e4b038056435ea3cd044beaf7cffe3d0f
Michael <mjsr@outlook.com>
2017-09-18 09:16:40 -0300

    Added hyperterm-cusor package

--- .hyper.js
+++ .hyper.js
@@ -104,7 +104,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-transparent', "hyper-dracula"],
+  plugins: ['hyper-transparent', "hyper-dracula", "hyperterm-cursor"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
