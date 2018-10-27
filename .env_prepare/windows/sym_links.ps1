659f21a12fdb6973cc7411e5b393a248479fda80
Michael <mjsr@outlook.com>
2018-10-27 18:56:51 -0300

    Added oni configuration file on windows environment Symlink creation was added sym_links script

--- .env_prepare/windows/sym_links.ps1
+++ .env_prepare/windows/sym_links.ps1
@@ -4,3 +4,6 @@ rm $PROFILE
 cmd /c "mklink $PROFILE $HOME\.core_terminal\Microsoft.PowerShell_profile.ps1"
 rm $HOME\.hyper.js
 cmd /c "mklink $HOME\.hyper.js $HOME\.env_prepare\windows\.hyper.js"
+rm C:\Users\Michael\AppData\Roaming\Oni\config.tsx
+cmd /c "mklink $ENV:APPDATA\Oni\config.tsx $HOME\.env_prepare\windows\config.tsx
+
