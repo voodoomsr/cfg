50650b9bbfc28648af8998526d9b5783e58fffb6
Michael <mjsr@outlook.com>
2019-02-24 14:39:04 -0300

    Updated cfg-install to force checkout master branch

--- .cfg_init/cfg-install.sh
+++ .cfg_init/cfg-install.sh
@@ -3,12 +3,12 @@ function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
-config checkout
+config checkout --force master
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
-config checkout
+config checkout --force master
 config config status.showUntrackedFiles no
